3b3dbd8ca2d7476cfc92bce485bf171016832725
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-04 17:21:12 +0100

    add getters and setters for user model

--- backend/src/main/java/com/codesync/backend/entity/User.java
+++ backend/src/main/java/com/codesync/backend/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="users")
@@ -11,6 +13,7 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 
+@Getter @Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
