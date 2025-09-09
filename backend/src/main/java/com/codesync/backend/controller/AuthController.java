e79bd9f7e0920db9e2ce409aa1c526556b596d88
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-09 19:45:13 +0100

    add login and register endpoints using auth service

---
+++ backend/src/main/java/com/codesync/backend/controller/AuthController.java
@@ -0,0 +1,50 @@
+package com.codesync.backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codesync.backend.model.dto.LoginRequestDto;
+import com.codesync.backend.model.dto.RegistrationRequestDto;
+import com.codesync.backend.model.dto.UserResponseDto;
+import com.codesync.backend.model.entity.Role;
+import com.codesync.backend.service.AuthService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegistrationRequestDto registrationRequest){
+        try {
+            UserResponseDto response = authService.register(registrationRequest);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDto dto){
+        try {
+            UserResponseDto response = authService.login(dto);
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }   
+    }
+}
