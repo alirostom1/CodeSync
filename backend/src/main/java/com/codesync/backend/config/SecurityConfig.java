48036626343be513717d0cd3f42329073e9659f3
ali-rostom1 <ali.rostom220@gmail.com>
2025-09-09 19:42:03 +0100

    add bcrypt paswwordEncoder bean in security config

--- backend/src/main/java/com/codesync/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/codesync/backend/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,5 +18,9 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable());
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder paPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
